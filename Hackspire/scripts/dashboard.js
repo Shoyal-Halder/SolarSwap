b5838bd26dfe737bd77936cb4dbe5ef09d2353ca
Shoyal Halder <shoyalhaldar49@gmail.com>
2024-12-28 22:48:28 +0530

    Delete Hackspire directory

--- Hackspire/scripts/dashboard.js
+++
@@ -1,7 +0,0 @@
-function redirectToBilling() {
-    window.location.href = "billing.html";
-}
-
-function redirectToCust_support() {
-    window.location.href = "cust_support.html";
-}
--- Hackspire/scripts/home.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const heading = document.getElementById("solarSwap");
-    setTimeout(() => {
-        heading.style.animation = "slideUp 2s ease-out forwards";
-        document.querySelector(".content").style.display = "block";
-    }, 2000);
-});
